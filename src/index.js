e4be0427990a62f8d5dc3824a2bfc910480b72fb
NoeliaVasquezCrespo <noelia.vasquez.crespo@gmail.com>
2026-03-09 17:48:02 -0400

    Correction in the project structure and implementation of error 404

--- src/index.js
+++ src/index.js
@@ -3,13 +3,19 @@ const dotenv = require('dotenv')
 
 const app = express();
 
-dotenv.config({ path: './src/.env' });
+dotenv.config({ path: './env' });
 
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send('Hola mundo');
 })
+
+app.use((req, res) => {
+    res.status(404).json({
+        message: 'Ruta no encontrada'
+    });
+});
 
 app.listen(port, () => {
     console.log(`Server corriendo en el puerto ${port}`)
