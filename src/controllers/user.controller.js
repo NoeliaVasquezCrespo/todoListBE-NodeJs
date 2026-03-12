2ca8f71b9f216693e8f4c998ac92e2991a9cb7e5
NoeliaVasquezCrespo <noelia.vasquez.crespo@gmail.com>
2026-03-12 13:13:43 -0400

    BTN-7: Authentication using jwt and bcrypt

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,7 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const db = require('../db/connection');
+const { userDecorator } = require('../decorators/user.decorator');
 
 const store = async (req, res) => {
     try {
@@ -11,15 +13,59 @@ const store = async (req, res) => {
 
         await db.execute(query, [name, email, hashedPassword]);
 
-        res.status(201).json({
-            name, email, hashedPassword
-        });
+        res.status(201).json(
+            userDecorator({ name, email })
+        );
 
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
 }
 
+const login = async (req, res) => {
+    try {
+
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return res.status(400).json({
+                message: "email y password son obligatorios"
+            });
+        }
+
+        const [users] = await db.execute(
+            `SELECT * FROM users WHERE email = ?`, [email]
+        );
+
+        const user = users[0];
+
+        if (!user) {
+            return res.status(401).json({ message: "Datos incorrectos" });
+        }
+
+        const validPassword = await bcrypt.compare(password, user.password);
+
+        if (!validPassword) {
+            return res.status(401).json({ message: "Datos incorrectos" });
+        }
+
+        const token = jwt.sign(
+            { id: user.id, email: user.email }, process.env.JWT_SECRET, { expiresIn: "1h" }
+        );
+
+        res.json({
+            accessToken: token,
+            user: userDecorator(user)
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        });
+    }
+};
+
 module.exports = {
-    store
+    store,
+    login
 };
