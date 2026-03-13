77035fe421ee9b251cd95eaeedd41c29bc48dbfa
NoeliaVasquezCrespo <noelia.vasquez.crespo@gmail.com>
2026-03-12 20:49:15 -0400

    BTN-5: Tags module with corrections

---
+++ src/db/migrations/003_create_tags.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS tags (
+    id VARCHAR(36) PRIMARY KEY,
+    name VARCHAR(250) UNIQUE NOT NULL,
+    description VARCHAR(250) NOT NULL,
+    color VARCHAR(8) NOT NULL,
+    created_at TIMESTAMP NULL,
+    updated_at TIMESTAMP NULL
+);
