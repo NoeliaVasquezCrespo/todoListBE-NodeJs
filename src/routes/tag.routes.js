551bcf569af1816a0006975e6a331f8a08d5f47f
NoeliaVasquezCrespo <noelia.vasquez.crespo@gmail.com>
2026-03-16 15:20:46 -0400

    BTN-8: Middleware implementation

--- src/routes/tag.routes.js
+++ src/routes/tag.routes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-
+const { verifyToken } = require('../middlewares/auth.middleware');
 const tagController = require('../controllers/tag.controller.js');
 
-router.get('/', tagController.index);
-router.get('/:id', tagController.show);
-router.post('/', tagController.store);
-router.put('/:id', tagController.update);
-router.delete('/:id', tagController.destroy);
+router.get('/', verifyToken, tagController.index);
+router.get('/:id', verifyToken, tagController.show);
+router.post('/', verifyToken, tagController.store);
+router.put('/:id', verifyToken, tagController.update);
+router.delete('/:id', verifyToken, tagController.destroy);
 
 module.exports = router;
